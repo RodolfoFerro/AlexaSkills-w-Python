1e97796f0dffa0c1e89c5bd80c41bf46de68307c
Rodolfo Ferro <rodolfoferroperez@gmail.com>
2017-12-22 04:09:55 -0600

    Created gitignore. Created credentials. Updated reddit script.

--- reddit_python_news.py
+++ reddit_python_news.py
@@ -6,9 +6,14 @@
 # Alexa skill to gather Python news from Reddit
 # =========================================================
 
-from flask import Flask
 from flask_ask import Ask, statement, question, session
-import requests, json, unidecode, time, logging
+from credentials import *
+from flask import Flask
+import unidecode
+import requests
+import logging
+import json
+import time
 
 # Set global variables
 name = 'Rodolfo'
@@ -18,20 +23,24 @@ log.addHandler(logging.StreamHandler())
 log.setLevel(logging.DEBUG)
 logging.getLogger("flask_ask").setLevel(logging.DEBUG)
 
+
 # Create Flask, Ask apps
-app = Flask(__name__) # Standard Flask app
-ask = Ask(app, "/reddit_python") # App endpoint
+app = Flask(__name__)  # Standard Flask app
+ask = Ask(app, "/reddit_python")  # App endpoint
+
 
 # App route
 @app.route("/")
 def homepage():
     return "Hey there {}! Flask is running with no problems!".format(name)
 
+
 # Alexa initial message (starting app...)
 @ask.launch
 def start_skill():
-    welcome_msg = "Hello there {}, would you like to hear any Python toppics on Reddit?".format(name)
+    welcome_msg = "Hi {}, would you like to hear any Python toppics on Reddit?".format(name)
     return question(welcome_msg)
+
 
 # If answer is yes
 @ask.intent("YesIntent")
@@ -40,11 +49,13 @@ def share_headlines():
     headline_msg = "The current Python headlines on Reddit are: {}.".format(python_headlines)
     return statement(headline_msg)
 
+
 # If answer is no
 @ask.intent("NoIntent")
 def no_intent():
     bye_text = "Okay, good bye."
     return statement(bye_text)
+
 
 # End session
 @ask.session_ended
@@ -52,12 +63,13 @@ def session_ended():
     log.debug("Session ended.")
     return "", 200
 
+
 # Consume Reddit API
 def reddit_python_headlines():
     # Login url and metadata
     reddit_url = "https://www.reddit.com/api/login"
-    user_pass_dict = {'user': 'INSERT_REDDIT_USER_HERE',
-                      'passwd': 'INSERT_REDDIT_PASS_HERE',
+    user_pass_dict = {'user': REDDIT_USER,
+                      'passwd': REDDIT_PASSWORD,
                       'api_type': 'json'}
 
     # Create a session
@@ -67,13 +79,15 @@ def reddit_python_headlines():
     time.sleep(1)   # Wait for connection...
 
     # Consume Reddit's API to gather info
-    url  = "https://www.reddit.com/r/Python/.json?limit=10"
+    url = "https://www.reddit.com/r/Python/.json?limit=10"
     html = sess.get(url)
     data = json.loads(html.content.decode('utf-8'))
-    titles = [ unidecode.unidecode(listing['data']['title']) for listing in data['data']['children'] ]
+    lst = listing['data']['title']
+    titles = [unidecode.unidecode(lst) for listing in data['data']['children']]
     titles = "... ".join([title for title in titles])
 
     return titles
+
 
 if __name__ == "__main__":
     app.run(debug=True)
